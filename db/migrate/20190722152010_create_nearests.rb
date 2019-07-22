85e98b61c360425f35f383b830e053c9723ba1db
鈴木竜生 <suzukiryusei@RYS-Macbook.local>
2019-07-23 00:20:41 +0900

    add:最寄り駅情報のモデル作成

---
+++ db/migrate/20190722152010_create_nearests.rb
@@ -0,0 +1,12 @@
+class CreateNearests < ActiveRecord::Migration[5.0]
+  def change
+    create_table :nearests do |t|
+      t.references :rent, foreign_key: true
+      t.string :route_name
+      t.string :station
+      t.integer :time
+
+      t.timestamps
+    end
+  end
+end
