6420322d69480272702877d1de0d2f4994967c7a
鈴木竜生 <suzukiryusei@RYS-Macbook.local>
2019-07-27 02:06:01 +0900

    add:登録物件削除機能追加

--- app/controllers/propertys_controller.rb
+++ app/controllers/propertys_controller.rb
@@ -1,4 +1,5 @@
 class PropertysController < ApplicationController
+before_action :set_property, only:[:show,:edit,:update,:destroy]
   def index
     @property = Property.all
   end
@@ -18,9 +19,21 @@ class PropertysController < ApplicationController
     end
   end
 
+  def destroy
+    @property.destroy
+    #redirect_to pictures_path, notice: "ブログを作成しました！"
+    redirect_to propertys_path
+  end
+
   private
+
   def property_params
     params.require(:property).permit(:name,:price,:address,:age,:remarks,
       nearests_attributes: [:id,:route_name,:station,:time])
   end
+
+  def set_property
+    @property = Property.find(params[:id])
+  end
+
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,4 @@
 class Property < ApplicationRecord
-  has_many :nearests
+  has_many :nearests, dependent: :destroy
   accepts_nested_attributes_for :nearests
 end
